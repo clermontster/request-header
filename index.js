e34b1c21c1c48e9524082bec2899e8d713372b9e
Jonnie Clermont <clermontster@gmail.com>
2017-10-25 13:22:14 -0500

    update ip address retrieval

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const port = process.env.PORT || 3000;
 
 const app = http.createServer((req,res) => {
 	res.writeHead(200, {'Content-type': 'text/plain'});
-	
-	const os = req.headers['user-agent'].split('(')[1].split(')')[0];
+
+	const system = req.headers['user-agent'].split('(')[1].split(')')[0];
 	const user = {
-		ip: req.connection.remoteAddress,
+		ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
 		language: req.headers['accept-language'].split(',')[0],
-		os: os,
+		os: system,
 	}
 	res.end(JSON.stringify(user));
 });
